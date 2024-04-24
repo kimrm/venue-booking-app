fee6204045e51675ccd3439a3055e517ffbacd5c
Kim Rune Møller <kimrune@hotmail.no>
2024-04-24 16:34:51 +0200

    adds UI context to keep track of things like modal open state, etc.

---
+++ src/context/UiContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useEffect, useMemo, useState } from "react";
+
+export interface UiContextType {
+	searchModalOpen: boolean;
+	setSearchModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const UiContext = createContext<UiContextType | undefined>(undefined);
+
+interface Props {
+	children: React.ReactNode;
+}
+
+export default function UiContextProvider({ children }: Props) {
+	const [searchModalOpen, setSearchModalOpen] = useState<boolean>(false);
+	const contextValue: UiContextType = useMemo(() => {
+		return { searchModalOpen, setSearchModalOpen };
+	}, [searchModalOpen, setSearchModalOpen]);
+
+	return (
+		<UiContext.Provider value={contextValue}>{children}</UiContext.Provider>
+	);
+}
