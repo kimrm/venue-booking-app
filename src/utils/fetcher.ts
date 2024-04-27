91650cb6f19ef8359a847c2420c940804827fbe3
Kim Rune Møller <kimrune@hotmail.no>
2024-04-27 19:32:35 +0200

    adds optional post data

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,3 +1,10 @@
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = async (url: string, method?: string, postData?: any) =>
+	fetch(url, {
+		method: method || "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: postData ? JSON.stringify(postData) : null,
+	}).then((res) => res.json());
 
 export default fetcher;
