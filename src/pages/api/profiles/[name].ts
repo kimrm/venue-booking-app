a382ae9f31086da8f469f739c4f4a5f9a81c6181
Kim Rune Møller <kimrune@hotmail.no>
2024-04-28 17:28:57 +0200

    profile api

---
+++ src/pages/api/profiles/[name].ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+	status: string;
+};
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<ResponseData>
+) {
+	const userName = req.cookies.username;
+
+	const accessToken = req.cookies.accesstoken;
+
+	if (!accessToken) {
+		res.status(401).json({ status: "Unauthorized" });
+		return;
+	}
+
+	const api_url = `https://v2.api.noroff.dev/holidaze/profiles/${userName}`;
+	const API_KEY = "74f572d2-19b2-4919-8edd-45508b626fed";
+
+	const fetchResponse = await fetch(api_url, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+			"X-Noroff-API-Key": API_KEY,
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
+
+	const data = await fetchResponse.json();
+
+	if (fetchResponse.ok) {
+		res.status(200).json(data.data);
+		return;
+	}
+
+	res.status(401).json(data);
+}
