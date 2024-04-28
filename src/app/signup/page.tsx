e1f5a19bb8fbe9eaf3228269383081480a92eaf0
Kim Rune Møller <kimrune@hotmail.no>
2024-04-28 16:53:33 +0200

    adds framer motion, success message component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import RegisterForm from "@/components/register";
 import { usePostFetch } from "@/hooks/api";
+import { motion } from "framer-motion";
 
 type RegisterData = {
 	name: string;
@@ -16,10 +17,13 @@ export default function SignupPage() {
 	const [signupData, setSignupData] = useState<RegisterData | null>(null);
 	const { data, loading, error } = usePostFetch("/api/register", signupData);
 	const [errorFeedback, setErrorFeedback] = useState<string | null>(null);
+	const [isSuccessFullRegistration, setIsSuccessFullRegistration] =
+		useState<boolean>(false);
 
 	useEffect(() => {
 		if (!data) return;
 		console.log("usePostFetch data: ", data);
+		setIsSuccessFullRegistration(true);
 	}, [data]);
 
 	useEffect(() => {
@@ -42,8 +46,51 @@ export default function SignupPage() {
 	}, [error]);
 
 	function handleRegisterCallback(registerData: RegisterData) {
-		console.log("registerData: ", registerData);
+		console.log(registerData);
 		setSignupData(registerData);
+	}
+
+	if (isSuccessFullRegistration) {
+		return (
+			<div className="flex min-h-full flex-grow items-center ">
+				<div className="mx-auto w-full bg-slate-100 p-10 sm:rounded-xl lg:mx-auto lg:w-1/2">
+					<h1 className="mb-3 font-serif text-2xl font-bold">
+						Sign up to start your journey
+					</h1>
+					<h2 className="mb-10">
+						... or start listing your home for rent and make money.
+					</h2>
+					<motion.div
+						initial={{ opacity: 0, y: -10 }}
+						animate={{ opacity: 1, y: 0 }}
+						className="my-10 flex items-center gap-4 font-bold text-green-500"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="h-10 w-10"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M5 13l4 4L19 7"
+							/>
+						</svg>
+
+						<p>Registration successful!</p>
+					</motion.div>
+					<p className="mt-5">
+						Go to{" "}
+						<a href="/login" className="text-orange-400">
+							Log in
+						</a>
+					</p>
+				</div>
+			</div>
+		);
 	}
 	return (
 		<div className="flex min-h-full flex-grow items-center ">
@@ -56,7 +103,11 @@ export default function SignupPage() {
 				</h2>
 				<RegisterForm submitCallback={handleRegisterCallback} error={error} />
 				{error && (
-					<div className="my-10 flex items-center gap-4 font-bold text-red-500">
+					<motion.div
+						initial={{ opacity: 0, y: -10 }}
+						animate={{ opacity: 1, y: 0 }}
+						className="my-10 flex items-center gap-4 font-bold text-red-500"
+					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -73,7 +124,7 @@ export default function SignupPage() {
 						</svg>
 
 						<p>{errorFeedback}</p>
-					</div>
+					</motion.div>
 				)}
 				<p className="mt-5">
 					Already have an account?{" "}
