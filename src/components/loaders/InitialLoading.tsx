e3720fb1610c14f42916b415bbf65d0bc5dd5a21
Kim Rune Møller <kimrune@hotmail.no>
2024-05-02 14:33:47 +0200

    adds initial loading screen

---
+++ src/components/loaders/InitialLoading.tsx
@@ -0,0 +1,9 @@
+export default function InitialLoading() {
+	return (
+		<div className="mt-32 flex h-screen min-h-screen w-full">
+			<div className="mx-auto animate-pulse text-sm font-bold uppercase tracking-widest text-yellow-600">
+				Loading ...
+			</div>
+		</div>
+	);
+}
