4fee934610d1905e3f3ee6ae7a2b56deafd8d795
Kim Rune Møller <kimrune@hotmail.no>
2024-05-06 01:51:10 +0200

    refactor

--- src/app/admin/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function layout({ children }: { children: React.ReactNode }) {
-	return (
-		<div>
-			<h1>Admin pages</h1>
-			<div>{children}</div>
-		</div>
-	);
-}
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-	return (
-		<footer className="z-10 mt-5 flex justify-center bg-gray-50 p-4">
-			Footer
-		</footer>
-	);
-}
