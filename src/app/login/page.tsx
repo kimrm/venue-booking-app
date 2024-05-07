538a1b0f2d69bd7d720d93aa78e04b085ace1db6
Kim Rune Møller <kimrune@hotmail.no>
2024-05-07 22:38:57 +0200

    refactor to use new app router api endpoints

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,13 @@
-import LoginForm from "@/components/LoginForm";
-import { permanentRedirect } from "next/navigation";
+import Form from "./_components/form";
 
 export default function LoginPage() {
-	async function success() {
-		"use server";
-		permanentRedirect("/");
-	}
 	return (
 		<div className="flex min-h-full flex-grow items-center ">
 			<div className="mx-auto w-full bg-slate-100 p-10 sm:rounded-xl lg:mx-auto lg:w-1/2">
 				<h1 className="mb-10 font-serif text-2xl font-bold">
 					Log in to start your journey
 				</h1>
-				<LoginForm success={success} />
+				<Form />
 				<p className="mt-5">
 					Don&apos;t have an account yet?{" "}
 					<a href="/signup" className="text-orange-400">
