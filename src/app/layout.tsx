77379fb3b454e9a881b76d4e020db7e5e128e379
Kim Rune Møller <kimrune@hotmail.no>
2024-05-07 22:36:49 +0200

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,59 +1,47 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import AppInitializer from "@/components/AppInitializer";
-import { cookies } from "next/headers";
 import "@fontsource-variable/playfair-display";
 import "@fontsource-variable/figtree";
-import { API_URL } from "@/vars/api";
-import { NoroffAPIRequest } from "@/types/Request";
 import { Suspense } from "react";
 import InitialLoading from "@/components/loaders/InitialLoading";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { getProfile } from "@/actions/profile";
+import UserProvider from "@/context/UserContext";
+import UiContextProvider from "@/context/UiContext";
+import DataContextProvider from "@/context/DataContext";
+import UserProfile from "@/types/UserProfile";
 
 export const metadata: Metadata = {
 	title: "Holidation",
 	description: "Rent a venue for your next event",
 };
 
-async function getProfile() {
-	const accessToken = cookies().get("accesstoken");
-	const userName = cookies().get("username");
-	let logins = cookies().get("logins");
-
-	if (accessToken && userName) {
-		const options: NoroffAPIRequest = {
-			method: "GET",
-			headers: {
-				Authorization: `Bearer ${accessToken.value}`,
-				"X-Noroff-API-Key": process.env.API_KEY,
-			},
-		};
-		const response = await fetch(
-			`${API_URL}/profiles/${userName.value}`,
-			options
-		);
-		const data = await response.json();
-
-		data.data.logins = logins?.value ?? 0;
-
-		return data.data;
-	}
-	return undefined;
-}
-
 export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const profile = await getProfile();
+	const profile: UserProfile = await getProfile();
 
 	return (
 		<html lang="en">
 			<body className="min-h-screen">
-				<AppInitializer userProfile={profile}>{children}</AppInitializer>
+				<div className="container mx-auto px-4">
+					<div className="flex h-screen flex-col">
+						<UserProvider initialData={profile}>
+							<UiContextProvider>
+								<DataContextProvider>
+									<Header />
+									<Suspense fallback={<InitialLoading />}>
+										<main className="flex-grow">{children}</main>
+									</Suspense>
+									<Footer />
+								</DataContextProvider>
+							</UiContextProvider>
+						</UserProvider>
+					</div>
+				</div>
 			</body>
 		</html>
 	);
