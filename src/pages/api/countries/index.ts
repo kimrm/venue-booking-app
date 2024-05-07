c1f9b3b1a7b942c9a86a08761be0db24a203e5e1
Kim Rune Møller <kimrune@hotmail.no>
2024-05-07 22:33:04 +0200

    refactor

--- src/pages/api/countries/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { countries } from "@/vars/countries";
-
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-) {
-	res.status(200).json(countries);
-}
