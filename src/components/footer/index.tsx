eaff301e2c457c699439823f724b522837759b80
Kim Rune Møller <kimrune@hotmail.no>
2024-05-13 17:42:33 +0200

    adds footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function Footer() {
+	const year = new Date().getFullYear();
 	return (
-		<footer className="z-10 mt-5 flex justify-center bg-gray-50 p-4">
-			Footer
+		<footer className="z-10 mt-5 flex w-screen flex-col justify-center bg-gray-900 p-4">
+			<p className="my-10 text-center font-serif text-lg text-gray-50">
+				At Holidation, we believe in the power of travel to change the world.
+			</p>
+			<p className="text-center text-sm text-gray-50">
+				&copy; {year} Holidation. All rights reserved.
+			</p>
 		</footer>
 	);
 }
