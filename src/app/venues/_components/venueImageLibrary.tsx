49e7f814809f2bba6d5878cb926ec18f84209d1c
Kim Rune Møller <kimrune@hotmail.no>
2024-05-19 15:37:27 +0200

    refactor

--- src/app/venues/_components/venueImageLibrary.tsx
+++ src/app/venues/_components/venueImageLibrary.tsx
@@ -2,24 +2,36 @@
 import Image from "next/image";
 import Venue from "@/types/Venue";
 
-export default function VenueImageLibrary({ venue }: { venue: Venue }) {
+interface Props {
+	venue: Venue;
+	expandedClicked: (index: number) => void;
+}
+export default function VenueImageLibrary({ venue, expandedClicked }: Props) {
 	return (
-		<div className="flex w-full flex-wrap items-center gap-2 lg:items-start">
-			{venue.media &&
-				venue.media.length > 0 &&
-				venue.media?.map((media) => {
-					return (
-						<Image
-							key={media.id}
-							src={media.url}
-							alt={media.alt}
-							width={200}
-							height={200}
-							title={media.alt}
-							className="mx-auto h-40 w-40 rounded-lg object-cover xl:mx-0 "
-						/>
-					);
-				})}
+		<div className="rounded bg-gray-100 p-4">
+			<h3 className="mb-5 text-sm uppercase tracking-wide">
+				Images from {venue.name}
+			</h3>
+			<div className="flex items-center justify-between">
+				<div className="mx-auto grid grid-cols-2 flex-wrap gap-4 sm:grid-cols-4 lg:grid-cols-3">
+					{venue.media &&
+						venue.media.length > 0 &&
+						venue.media?.map((media, index) => {
+							return (
+								<Image
+									key={media.id}
+									src={media.url}
+									alt={media.alt}
+									width={200}
+									height={200}
+									title={media.alt}
+									onClick={() => expandedClicked(index)}
+									className="h-40 w-full cursor-pointer rounded-lg object-cover sm:w-40"
+								/>
+							);
+						})}
+				</div>
+			</div>
 		</div>
 	);
 }
