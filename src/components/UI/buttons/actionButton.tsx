10cf905de101dfd0962b13ed1b5de46fcf5f55d5
Kim Rune Møller <kimrune@hotmail.no>
2024-05-21 00:07:06 +0200

    adds disabled classes

--- src/components/UI/buttons/actionButton.tsx
+++ src/components/UI/buttons/actionButton.tsx
@@ -10,7 +10,7 @@ export default function ActionButton({
 	return (
 		<button
 			{...rest}
-			className={`flex items-center gap-1 rounded-lg bg-yellow-400 px-4 py-2 text-xs uppercase tracking-wide hover:bg-yellow-300 ${transition}`}
+			className={`flex items-center gap-1 rounded-lg bg-yellow-400 px-4 py-2 text-xs uppercase tracking-wide hover:bg-yellow-300 disabled:animate-pulse disabled:bg-yellow-200 disabled:text-yellow-600 ${transition}`}
 		>
 			{children}
 		</button>
