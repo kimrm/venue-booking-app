6bfef891c5cb14b665d5c50dec0b57b459753ca5
Kim Rune Møller <kimrune@hotmail.no>
2024-05-22 15:56:16 +0200

    adds delete venue

--- src/components/profile/venues/edit/index.tsx
+++ src/components/profile/venues/edit/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Venue from "@/types/Venue";
 import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { update } from "@/actions/venues";
+import { update, destroy } from "@/actions/venues";
 import { useFormStatus, useFormState } from "react-dom";
 import { Input, TextArea } from "@/components/form";
 import { SubmitButton } from "@/components/UI/buttons";
@@ -106,6 +106,18 @@ export default function Edit({ venue }: Props) {
 		}
 		return () => clearTimeout(timeout);
 	}, [state]);
+
+	async function handleDelete() {
+		const result = confirm("Are you sure you want to delete this venue?");
+		if (result) {
+			if (venue.id) {
+				const data = await destroy(venue.id);
+				if (data.status === "ok") {
+					window.location.href = "/profile/venues";
+				}
+			}
+		}
+	}
 
 	return (
 		<>
@@ -285,8 +297,8 @@ export default function Edit({ venue }: Props) {
 							: venue?.location?.continent
 					}
 				/>
-				<input type="hidden" name="lat" defaultValue={location.latitude} />
-				<input type="hidden" name="lng" defaultValue={location.longitude} />
+				<input type="text" name="lat" defaultValue={location.latitude} />
+				<input type="text" name="lng" defaultValue={location.longitude} />
 
 				<div className="mb-5">
 					<span className="block text-sm uppercase tracking-wide">Images</span>
@@ -325,8 +337,30 @@ export default function Edit({ venue }: Props) {
 						</div>
 					</div>
 				</div>
-				<div>
+				<div className="flex justify-between">
 					<SubmitButton>Save</SubmitButton>
+					<button
+						type="button"
+						onClick={handleDelete}
+						title="Delete venue"
+						className="flex gap-2 rounded-xl border border-red-200 p-2 text-red-600 hover:bg-red-100"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="h-6 w-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+							/>
+						</svg>
+						Delete venue
+					</button>
 				</div>
 				<input
 					type="hidden"
