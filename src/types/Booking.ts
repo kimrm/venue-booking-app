5276cc062b5aabeff6057d8c4a284064950785cf
Kim Rune Møller <kimrune@hotmail.no>
2024-05-25 01:10:21 +0200

    adds Booking type

---
+++ src/types/Booking.ts
@@ -0,0 +1,21 @@
+import Venue from "./Venue";
+
+interface Booking {
+	id: string;
+	dateFrom: string;
+	dateTo: string;
+	guests: number;
+	created: string;
+	updated: string;
+	venue?: Venue;
+	customer: {
+		name: string;
+		email: string;
+		avatar?: {
+			url: string;
+			alt: string;
+		};
+	};
+}
+
+export default Booking;
