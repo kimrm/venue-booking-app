b4e62567901a2461bc7fd01dfdc598130ef76e57
Kim Rune Møller <kimrune@hotmail.no>
2024-05-26 21:58:08 +0200

    gets profile context and conditional renders navigation

--- src/app/profile/_components/navigation.tsx
+++ src/app/profile/_components/navigation.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React from "react";
+import { useContext } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { UserContext } from "@/context/UserContext";
 
 export default function Navigation() {
 	const pathname = usePathname();
+	const { profile } = useContext(UserContext) || {};
 
 	return (
 		<ul className="py-2">
@@ -25,34 +27,36 @@ export default function Navigation() {
 					Bookings
 				</Link>
 			</li>
-			<li className={`mb-2 p-4`}>
-				<Link
-					className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/venues" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
-					href="/profile/venues"
-				>
-					Venues
-				</Link>
-				{pathname.includes("/profile/venues") && (
-					<ul className="mt-5">
-						<li className={`mb-4 px-4`}>
-							<Link
-								className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/venues/register" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
-								href="/profile/venues/register"
-							>
-								Register a venue
-							</Link>
-						</li>
-						<li className={`px-4`}>
-							<Link
-								className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/venues/bookings" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
-								href="/profile/venues/guests"
-							>
-								Guests
-							</Link>
-						</li>
-					</ul>
-				)}
-			</li>
+			{profile?.venueManager && (
+				<li className={`mb-2 p-4`}>
+					<Link
+						className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/venues" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
+						href="/profile/venues"
+					>
+						Venues
+					</Link>
+					{pathname.includes("/profile/venues") && (
+						<ul className="mt-5">
+							<li className={`mb-4 px-4`}>
+								<Link
+									className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/venues/register" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
+									href="/profile/venues/register"
+								>
+									Register a venue
+								</Link>
+							</li>
+							<li className={`px-4`}>
+								<Link
+									className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/venues/bookings" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
+									href="/profile/venues/guests"
+								>
+									Guests
+								</Link>
+							</li>
+						</ul>
+					)}
+				</li>
+			)}
 
 			<li className={`mb-2 p-4`}>
 				<Link
@@ -70,6 +74,16 @@ export default function Navigation() {
 					Avatar
 				</Link>
 			</li>
+			{!profile?.venueManager && (
+				<li className="mb-2 p-4">
+					<Link
+						className={`mx-1 block whitespace-nowrap rounded outline-offset-4 outline-yellow-400 ${pathname === "/profile/become-venue-manager" ? "text-gray-950" : "text-gray-600"} hover:text-black`}
+						href="/profile/become-venue-manager"
+					>
+						Add a Venue
+					</Link>
+				</li>
+			)}
 		</ul>
 	);
 }
