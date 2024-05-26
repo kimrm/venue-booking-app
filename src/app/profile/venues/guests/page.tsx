d701f6ad7360aab95214311062ecc6fdf13dbd53
Kim Rune Møller <kimrune@hotmail.no>
2024-05-26 02:25:50 +0200

    fixes responsiveness

--- src/app/profile/venues/guests/page.tsx
+++ src/app/profile/venues/guests/page.tsx
@@ -1,21 +1,21 @@
 import { getAllBookings } from "@/actions/bookings";
 import Link from "next/link";
-import Image from "next/image";
 
 export default async function Guests() {
 	const bookings = await getAllBookings();
 	return (
 		<div>
 			<h2 className="mb-3 border-spacing-2 border-spacing-y-24 border-b border-dashed border-b-gray-200  pb-2 text-xs font-bold uppercase text-gray-700">
-				Guests arriving
+				Next guests arriving
 			</h2>
-			<table className=" w-full table-auto rounded bg-gray-100">
+			<table className=" table w-full rounded bg-gray-100">
 				<thead className=" border-b-2 border-dashed">
 					<tr>
 						<th className="p-4 text-left">Guest</th>
-
 						<th className="p-4 text-left">Arrives</th>
-						<th className="w-16 max-w-20 p-4 text-right">Guests</th>
+						<th className="hidden w-16 max-w-20 p-4 text-right sm:block">
+							Guests
+						</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -30,26 +30,13 @@ export default async function Guests() {
 								key={booking.id}
 								className={`${index % 2 === 0 && "bg-white"}`}
 							>
-								<td className=" p-4 text-left">
-									<div className="flex items-center gap-2">
-										{booking?.customer?.avatar && (
-											<div className="inline">
-												<Image
-													src={booking.customer.avatar.url ?? ""}
-													alt={booking.customer.avatar.alt ?? ""}
-													className="hidden h-6 w-6 cursor-pointer rounded-full object-cover object-center lg:block"
-													width={25}
-													height={25}
-												/>
-											</div>
-										)}
-										<Link
-											className="text-sm uppercase tracking-wide hover:text-yellow-600"
-											href={`/profile/venues/guests/${booking.id}`}
-										>
-											{booking.customer?.name}
-										</Link>
-									</div>
+								<td className="p-2 text-left">
+									<Link
+										className="text-sm uppercase tracking-wide hover:text-yellow-600"
+										href={`/profile/venues/guests/${booking.id}`}
+									>
+										{booking.customer?.name}
+									</Link>
 									<Link
 										className=" mt-3 block overflow-hidden overflow-ellipsis whitespace-nowrap hover:text-yellow-600"
 										href={`/profile/venues/guests/${booking.id}`}
@@ -58,10 +45,10 @@ export default async function Guests() {
 									</Link>
 								</td>
 
-								<td className="p-4">
+								<td className="p-2 align-top">
 									{new Date(booking.dateFrom).toLocaleDateString()}
 								</td>
-								<td className="w-16 max-w-20 p-4 text-right">
+								<td className="hidden w-16 max-w-20 p-2 text-right align-top sm:block">
 									{booking.guests}
 								</td>
 							</tr>
